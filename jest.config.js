6ef5ebd6244bbf677aff82cd9f668537669e250a
Paul van Woensel <paulvanwoensel@Pauls-Mac-mini.local>
2021-02-03 11:02:20 -0600

    collecting coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,12 @@ module.exports = {
     ".(ts|tsx)": "ts-jest"
   },
   "moduleFileExtensions": ["ts", "tsx", "js"],
+  "collectCoverageFrom": [
+    "**/lib/**/*.{ts}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ],
+  collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "node",
